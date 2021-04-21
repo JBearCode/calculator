91c9db5176eb5c42316313d55fd8cf4a2b7eb2e5
JBearCode <joelbmoran@gmail.com>
2021-04-21 08:21:41 -0400

    Convert to only manipulate display value from main function

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 const screen = document.getElementById('screen');
 
 function operate(operator, num1, num2) {
-    return operator(num1, num2);
+    let newDisplayValue = operator(num1, num2);
+    screen.value = newDisplayValue;
 }
 
 function add(num1, num2) {
-    screen.value = num1 + num2;
+	return parseInt(num1, 10) + parseInt(num2, 10);
 }
 
 function subtract(num1, num2) {
-    screen.value = num1 - num2;
+	return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    screen.value = num1 * num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
     if (num2 == 0) {
         return;
-    } else screen.value = num1 / num2;
+    } else return num1 / num2;
 }
 
 function negate(num) {
     if (num == 0) {
         return;
     } else if (num < 0) {
-        screen.value = Math.abs(num);
+        return Math.abs(num);
     } else if (num > 0) {
-        screen.value = -Math.abs(num);
+        return -Math.abs(num);
     }
 }
 
 function square(num) {
-	screen.value = num * num;
+	return num * num;
 }
 
 function root(num) {
     if (num < 0) {
         return;
-    } else screen.value = Math.sqrt(num);
+    } else return Math.sqrt(num);
 }
 
