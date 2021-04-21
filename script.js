0a94090ec85bb48b3e66176e77ca9404fbb3b993
JBearCode <joelbmoran@gmail.com>
2021-04-21 08:17:21 -0400

    Convert functions to manipulate display value

--- script.js
+++ script.js
@@ -5,40 +5,40 @@ function operate(operator, num1, num2) {
 }
 
 function add(num1, num2) {
-	return parseInt(num1, 10) + parseInt(num2, 10);
+    screen.value = num1 + num2;
 }
 
 function subtract(num1, num2) {
-	return num1 - num2;
+    screen.value = num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    screen.value = num1 * num2;
 }
 
 function divide(num1, num2) {
     if (num2 == 0) {
         return;
-    } else return num1 / num2;
+    } else screen.value = num1 / num2;
 }
 
 function negate(num) {
     if (num == 0) {
         return;
     } else if (num < 0) {
-        return Math.abs(num);
+        screen.value = Math.abs(num);
     } else if (num > 0) {
-        return -Math.abs(num);
+        screen.value = -Math.abs(num);
     }
 }
 
 function square(num) {
-	return num * num;
+	screen.value = num * num;
 }
 
 function root(num) {
     if (num < 0) {
         return;
-    } else return Math.sqrt(num);
+    } else screen.value = Math.sqrt(num);
 }
 
